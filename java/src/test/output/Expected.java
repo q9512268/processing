c72c81a3973fe17eb206f32a8eb8464af7f71b91
Christian Lütticke <christianluetticke@ocean.fritz.box>
2020-09-30 17:17:56 +0200

    #2 processing-java - Sketch.pde-File und Expected.java in test-Folder hinzugefügt

---
+++ java/src/test/output/Expected.java
@@ -0,0 +1,32 @@
+package test.output;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Expected extends PApplet {
+
+public void setup(){
+  println("Hello, World!");
+}
+
+public void draw(){
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Sketch" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
